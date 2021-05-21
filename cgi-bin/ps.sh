b305979f2e7c11c84ab03a38c3034d0eb4710716
inyestar <yuinye@gmail.com>
2021-05-21 17:55:55 +0900

    process list pagination

--- cgi-bin/ps.sh
+++ cgi-bin/ps.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
+echo -e "Content-type: text/html\n\n"
 
 # print all processes
-ps -o uid= -o pid= -o ppid= -o stime= -o tty= -o time= -o cmd= -e
+# ps -o uid:1= -o pid:1= -o ppid:1= -o stime:1= -o tty:1= -o time:1= -o cmd:1= -e
+START=$(echo $QUERY_STRING | cut -d'&' -f1 | sed 's/start=//g') 
+END=$(echo $QUERY_STRING | cut -d'&' -f2 | sed 's/end=//g') 
+ps -o uid:1= -o pid:1= -o ppid:1= -o stime:1= -o tty:1= -o time:1= -o cmd:1= -e > ./pslist;
+sed -n ${START},${END}p ./pslist
+
+
--- html/resources/index.js
+++ html/resources/index.js
@@ -2,8 +2,7 @@ function ajax(url, elem) {
   var xhttp = new XMLHttpRequest();
    xhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
-        //elem.innerHTML = this.responseText;
-        beuatify(text);
+        elem.innerHTML = beautify(this.responseText);
        }
      };
 
@@ -17,11 +16,29 @@ function mock(url, elem) {
   console.log(result)
 }
 
-function beuatify(text) {
-    console.log(test)
+function beautify(text) {
+  let lines = text.split('\n');
+  let html = '';
+  for(let i=0; i<lines.length; i++) {
+    if(lines[i].trim().length == 0) {
+      continue;
+    }
+    html += '<tr>';
+    let columns = lines[i].split(' ');
+    for(let j=0; j<columns.length; j++) {
+      if(j===7) {
+         break;
+      }
+      html += '<td>';
+      html += columns[j];
+      html += '</td>';
+    }
+    html += '</tr>';
+  }
+ return html; 
 }
 
-function printProcess() {
+function printProcess(start, end) {
   let tbody = document.getElementById('ps-result');
-  mock('ps.sh', tbody);
+  ajax('ps.sh?start=' + start + '&end=' + end, tbody);
 }
