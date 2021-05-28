d701f65a6a8a5cad8120bdeb7fafa3abf9f7ad2d
inyestar <yuinye@gmail.com>
2021-05-28 09:11:10 +0900

    cgi 실행결과 디렉토리 변경

--- cgi-bin/ps.sh
+++ cgi-bin/ps.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 echo -e "Content-type: text/html\n\n"
 
-# print all processes
-# ps -o uid:1= -o pid:1= -o ppid:1= -o stime:1= -o tty:1= -o time:1= -o cmd:1= -e
+# print all process
 START=$(echo $QUERY_STRING | cut -d'&' -f1 | sed 's/start=//g')
 END=$(echo $QUERY_STRING | cut -d'&' -f2 | sed 's/end=//g')
-ps -o uid:1= -o pid:1= -o ppid:1= -o stime:1= -o tty:1= -o time:1= -o cmd:1= -e > ./pslist;
-echo "count="$(wc -l < ./pslist);
-sed -n ${START},${END}p ./pslist;
+ps -o uid:1= -o pid:1= -o ppid:1= -o stime:1= -o tty:1= -o time:1= -o cmd:1= -e > ./result/pslist;
+echo "count="$(wc -l < ./result/pslist);
+sed -n ${START},${END}p ./result/pslist;
